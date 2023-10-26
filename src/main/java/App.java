de4bcebcf352fa985a862458f26deca91d440481
VadimDariy <Metallika_1979@mail.ru>
2023-10-27 01:19:48 +0300

    Java pre-project. Практическая задача 2.1.3 Студент Дарий Вадим.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,46 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat = applicationContext.getBean("catBean", Cat.class);
+        cat.setName("Барсик");
+        Cat cat1 = applicationContext.getBean("catBean", Cat.class);
+        cat1.setName("Тимка");
+
+        System.out.println("Одинаковые ли ссыки cat и cat1? " + (cat==cat1));
+        System.out.println("Одинаковые ли объекты bean и bean1? " + (bean.equals(bean1)));
+        System.out.println("Имя объекта cat: " + cat.getName());
+        System.out.println("Имя объекта cat1: " + cat1.getName());
+
     }
 }
+
+
+/*
+У меня есть три класса:
+
+1. class App
+2. class HelloWorld
+3. class AppConfig
+ которые вкубе мне дают результат "Hello World!".
+
+Правильно ли я понимаю взаимосвязь и работу этих классов?
+Итак:
+В class App создаётся объект  "applicationContext " типа " ApplicationContext", конструктор которого у себя в параметрах
+принимает настройки конфигурационного класса "class AppConfig", который помечен аннотацией "@Configuration", которая
+означает, что данный класс является конфигурационным, настраиваемым для работы фреймворка Spring.  В этом классе есть
+метод "public HelloWorld getHelloWorld()", который возвращает строку "Hello World!" при следующих условиях:
+1. В "class App" получаем объект bean типа HelloWorld, который будет содержать в себе идинтификатор "helloWorld",
+   указывающий на ту часть кода с которым будет работать Spring.
+2. Указываем в  классе "AppConfig" над методом "public HelloWorld getHelloWorld()" аннотацию " @Bean(name="helloworld")",
+   в которой проводим инициализацию бина, который с помощью метода "setMessage("Hello World!")" из класса "Hello World!"
+   инициализирует переменную "message" значением "Hello World!"
+3. На объекте bean вызываем из класса "HelloWorld" метод getMessage(), который возвращает значение "Hello World!"
+   переменной "message" в консоль
+*/
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -6,9 +6,23 @@ import org.springframework.context.annotation.Scope;
 public class AppConfig {
  
     @Bean(name="helloworld")
+    //@Scope("singleton")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean
+    @Scope("prototype")
+    public Cat catBean(){
+        return new Cat();
+    }
 }
+
+/* Метод getHelloWorld() - создает и инициализирует объект HelloWorld внутри метода, который используется при каждом
+                           запросе к бину с именем "helloworld". Область видимости "singleton" означает, что Spring
+                           создает и инициализирует бин только один раз при старте контейнера, и затем этот один и тот
+                           же бин используется при каждом запросе.
+
+Метод catBean() - также создает объект Cat, но с областью видимости "prototype", что приводит к созданию нового объекта
+                  при каждом запросе к бину с именем "catBean", без дополнительной инициализации. */
